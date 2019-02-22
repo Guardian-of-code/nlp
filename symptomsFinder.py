8ea30df501c51a3dc17efef03d06ea964c10cdda
Simhadri G <simhadri064@gmail.com>
2019-02-22 22:22:02 +0530

    now can read input as a string

--- symptomsFinder.py
+++ symptomsFinder.py
@@ -1,27 +1,33 @@
-#import regular expressions
+#import regular  expressions
 import re
 import time
 
 
-#function to read a file, just pass the location of the message text file
-def readFile(path):
-    message =open(path)
+#function to read message as a string, just pass the message as a string.
+def readFile():
+
+    message = input("Enter your message:").split('. ')
+    
+    #print(message)
     return message
 
 
 #basic string comparison using real expression 
 def checkForSymptoms(message):
-    bagOfWords = ['chest pain','sweating','heart ache','fainting','nausea', 'sweat','cold sweat', 'fatigue', 'pain in chest', 'sudden dizziness','lethargy', 'tingling', 'numbness','seizures','difficulty seeing']
+    bagOfWords = ['chest pain','sweating','heart ache','fainting','nausea', 'sweat','cold sweat', 'fatigue', 'pain in chest', 'dizziness','lethargy', 'tingling', 'numbness','seizures','difficulty seeing']
     #can read it from a file , for now hardcoded.
 
     
     count = 0
     symptomsFound = []
-
+    
     # structure of location link = https://maps.google.com/maps?q=12.96544,77.531685
     for line in message:
+
+        #print(line.lower())
+        line = line.lower()
         count +=1
-        if count == 2:
+        if count == 1:
             locationURL = line.strip()
             locationOnMap = locationURL.split('=')
             lat,long = map(float,locationOnMap[1].split(','))
@@ -46,10 +52,10 @@ def checkForSymptoms(message):
 
 
 if __name__ == '__main__':
-    path = input('Enter the path of message file:')
+    #path = input('Enter the path of message file:')
 
     # read the message file
-    message = readFile(path)
+    message = readFile()
 
     
     startTime = time.time()#for performance analysis using time module, but not important.
@@ -59,4 +65,4 @@ if __name__ == '__main__':
     # retuns authenticity of message, location and symptoms as a dictionary
     symptoms = checkForSymptoms(message)
     print('\ndictionary returned =',symptoms)
-    print(time.time() - startTime)
+    #print(time.time() - startTime)
